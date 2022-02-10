6b02937464b1ccfd3f5578aded4784fc8c71060c
Goce Shapkaroski <goce.shapkaroski@opengi.mk>
2022-02-10 19:35:38 +0100

    Orders and RabbitMQ

---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Queries
+{
+    public class GetOrdersListQuery: IRequest<List<OrdersVm>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersListQuery(string username)
+        {
+            Username = username;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            Expression<Func<Order, bool>> predicate = o => o.UserName == userName;
+
+            var orderList = await this.GetAsync(predicate);
+
+            return orderList;
+        }
+    }
+}
